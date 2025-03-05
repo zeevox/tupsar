461a255b94a2fc86c2eb8c306a8cb6938f4debcd
Timothy Langer <zeevox.dev@gmail.com>
2025-03-05 10:55:38 +0000

    Add script to find unprocessed scans
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ scripts/find_missing.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+# "tqdm",
+# "natsort",
+# "rich",
+# ]
+# ///
+
+import argparse
+from pathlib import Path
+
+import natsort
+import rich
+
+
+def find_missing_scans(scans_dir: Path, articles_dir: Path) -> list[Path]:
+    """Find unprocessed scans lacking any extracted articles."""
+    articles = {
+        # split on second underscore
+        "_".join(article.stem.split("_")[:2])
+        for article in articles_dir.iterdir()
+    }
+
+    missing_scans = {scan for scan in scans_dir.iterdir() if scan.stem not in articles}
+
+    return natsort.natsorted(missing_scans)
+
+
+def cli() -> None:
+    """Handle command-line interface."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("scans_dir", type=str)
+    parser.add_argument("articles_dir", type=str)
+    args = parser.parse_args()
+
+    scans = Path(args.scans_dir)
+    articles = Path(args.articles_dir)
+
+    for folder in [scans, articles]:
+        if not folder.exists():
+            msg = f"Directory {folder} does not exist"
+            raise FileNotFoundError(msg)
+
+    for scan in find_missing_scans(scans, articles):
+        rich.print(scan)
+
+
+if __name__ == "__main__":
+    cli()
